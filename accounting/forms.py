f17e288638d4a56064304bf2e6683aa963000ae2
SarajZimba <sarajzimba@gmail.com>
2024-04-23 19:57:51 +0300

    category_added

--- accounting/forms.py
+++ accounting/forms.py
@@ -1,15 +1,23 @@
 from django import forms
-from .models import Expense
+from .models import Expense, ExpenseCategory
 
 class ExpenseForm(forms.ModelForm):
     class Meta:
         model = Expense
-        fields = ['title', 'amount', 'date', 'category', 'notes']
+        fields = ['title', 'amount', 'date', 'expense_category', 'notes']
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter title'}),
             'amount': forms.NumberInput(attrs={'class': 'form-control', 'placeholder': 'Enter amount'}),
             'date': forms.DateInput(attrs={'class': 'form-control', 'placeholder': 'Select date', 'type': 'date'}),
-            'category': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter category'}),
+            'expense_category': forms.Select(attrs={'class': 'form-select', "data-control":"select2"}),
             'notes': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter notes', 'rows': 3}),
         }
-
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.fields['expense_category'] = forms.ModelChoiceField(queryset=ExpenseCategory.objects.filter(is_deleted=False))
+    
+    #     self.fields["expense_category"].widget.attrs = {
+    #         "class":"form-select",
+    #         "data-control": "select2",
+    #         "data-placeholder": "Select Category",
+    #     }
--- accounting/views.py
+++ accounting/views.py
@@ -2,25 +2,30 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse, reverse_lazy
 from django.shortcuts import render
 from django.views.generic import View, UpdateView, DeleteView
-from .models import Expense
+from .models import Expense, ExpenseCategory
 from django.db.models import Sum
 from .forms import ExpenseForm
+
 
 class ExpenseFormView(View):
     template_name = 'expense/expense_form.html'
 
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name)
+        categories = ExpenseCategory.objects.filter(is_deleted=False)
+        context = {"categories": categories}
+        return render(request, self.template_name, context)
 
     def post(self, request, *args, **kwargs):
         titles = request.POST.getlist('title[]')
         amounts = request.POST.getlist('amount[]')
         dates = request.POST.getlist('date[]')
         notes = request.POST.getlist('note[]')
+        categories = request.POST.getlist('category[]')
 
         # Iterate through the submitted data to create Expense instances
-        for title, amount, date, note in zip(titles, amounts, dates,notes):
-            Expense.objects.create(title=title, amount=amount, date=date, notes=note)
+        for title, amount, date, note, category_id in zip(titles, amounts, dates,notes, categories):
+            category = ExpenseCategory.objects.get(pk=category_id)
+            Expense.objects.create(title=title, amount=amount, date=date, notes=note, expense_category=category)
 
         # Redirect to a success page or render a success message
         return HttpResponseRedirect(reverse('expense-list'))  # Replace 'success_page' with your actual URL name
@@ -31,9 +36,10 @@ class ExpenseList(View):
     def get(self, request, *args, **kwargs):
         expenses = Expense.objects.filter(is_deleted=False)
 
+        category_totals = Expense.objects.values('expense_category__title').annotate(total_amount=Sum('amount'))
         total_expense_amount = expenses.aggregate(total_amount=Sum('amount'))['total_amount']
 
-        context = {'expenses': expenses, 'total_expense': total_expense_amount}
+        context = {'expenses': expenses, 'total_expense': total_expense_amount, 'category_totals':category_totals}
         return render(request, self.template_name, context)
 
 class ExpenseUpdateView(UpdateView):
